b8813eaacd1b84df9e720fa492233ad3fe2ebbc6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-28 01:08:42 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,18 +15,6 @@ class Genre
     @@all
   end
   
-  def self.destroy_all
-    @@all.clear
-  end
-  
-  def save
-    @@all << self 
-  end
-  
-  def songs
-    @songs
-  end
-  
   def self.create(name)
     new_genre = Genre.new(name)
     new_genre.save
