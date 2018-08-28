e3878d8442ae9b233ac280ef74d1e1abf9827c3d
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-28 01:06:30 +0000

    automatically backed up by learn

--- lib/concerns/AllForAll.rb
+++ lib/concerns/AllForAll.rb
@@ -1,6 +1,15 @@
 module AllForAll
   module ClassMethods
-    
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def create(name)
+    new_artist = self.new(name)
+    new_artist.save
+    new_artist
+  end 
     
     
     
