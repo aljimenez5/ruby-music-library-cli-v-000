338722f65867b776c3829a3b7aaeaf7d2ac8b6ae
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-28 01:17:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,19 +28,6 @@ class Song
     @@all
   end
   
-  def save
-    @@all << self 
-  end
-  
-  def self.destroy_all
-    @@all.clear
-  end
-  
-  def self.create(name)
-    new_song = Song.new(name)
-    new_song.save
-    new_song
-  end
   
   def self.new_from_filename(file)
     filename = file.split(' - ')
