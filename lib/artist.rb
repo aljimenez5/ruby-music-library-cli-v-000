0598355f82db487a03fbfc5fde5822055d91c67a
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-28 01:16:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,20 +15,6 @@ class Artist
   def self.all 
     @@all
   end
-  
-  def save
-    @@all << self 
-  end
-  
-  def self.destroy_all
-    @@all.clear
-  end
-  
-  def self.create(name)
-    new_artist = Artist.new(name)
-    new_artist.save
-    new_artist
-  end
     
   def add_song(song)
     song.artist = self unless song.artist == self
